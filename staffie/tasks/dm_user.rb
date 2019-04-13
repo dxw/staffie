7194dad25447519270648a3963a871e129b4e119
F <f@dxw.com>
2019-04-13 17:05:30 +0100

    (chore) Make dm_user support blocks

--- staffie/tasks/dm_user.rb
+++ staffie/tasks/dm_user.rb
@@ -4,16 +4,25 @@ require 'slack-ruby-client'
 
 module Staffie
   module Tasks
-    def self.dm_user(user, message:, woof: true)
-      text = message.strip
-      text += ' Woof!' if message && woof
+    def self.dm_user(user, blocks: nil, message: nil, woof: true)
+      raise 'Must provide message or blocks' if message.nil? && blocks.nil?
+
+      post_message_options = {
+        channel: user.slack_user_id,
+        as_user: true
+      }
+
+      unless message.nil?
+        text = message.strip
+        text += ' Woof!' if message && woof
+
+        post_message_options[:text] = text.strip
+      end
+
+      post_message_options[:blocks] = blocks unless blocks.nil?
 
       client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
-      response = client.chat_postMessage(
-        channel: user.slack_user_id,
-        as_user: true,
-        text: text.strip
-      )
+      response = client.chat_postMessage(post_message_options)
 
       raise "Slack error: #{response.error}" unless response.ok
     end
