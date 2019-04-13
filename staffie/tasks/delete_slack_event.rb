b96769051be1a9c344f79b68d558dc7437bad801
F <f@dxw.com>
2019-04-13 16:51:41 +0100

    (feature) End snooze when destroying an event that covers now

--- staffie/tasks/delete_slack_event.rb
+++ staffie/tasks/delete_slack_event.rb
@@ -8,6 +8,13 @@ module Staffie
       slack_event = Models::SlackEvent.find_by!(id: id, user: user)
 
       slack_event.destroy!
+
+      return unless slack_event.date_range.cover?(DateTime.now)
+
+      client = Slack::Web::Client.new(token: user.slack_token)
+      response = client.dnd_endSnooze
+
+      raise "Slack error: #{response.error}" unless response.ok
     end
   end
 end
